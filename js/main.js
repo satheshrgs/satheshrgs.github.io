89766bcde7e5903b3601bbbde83a4a8584e7470c
Sathesh Rangasamy <sathesh.rangasamy@rksv.in>
2023-05-20 12:35:00 +0530

    Revamp to new cli

---
+++ js/main.js
@@ -0,0 +1,184 @@
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer");
+var textarea = document.getElementById("texter");
+var terminal = document.getElementById("terminal");
+
+var git = 0;
+var pw = false;
+let pwd = false;
+var commands = [];
+
+setTimeout(function () {
+  loopLines(banner, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+
+console.log(
+  "%cHey..!!, Thanks for Visiting - Sathesh Rgs",
+  "color: #04ff00; font-weight: bold; font-size: 24px;"
+);
+
+//init
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+  } else {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine("guest@satheshrgs.in:~$ " + command.innerHTML, "no-animation", 0);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+}
+
+async function getGithubRepos() {
+  try {
+    const resp = await fetch("https://api.github.com/users/satheshrgs/repos");
+    const data = await resp.json();
+    const projects = ["<br>"];
+
+    data.forEach((d) => {
+      projects.push(
+        `<span class="command">${d.name}</span> <br /><a href="${d.svn_url}"target="_blank">${d.svn_url}`
+      );
+    });
+    projects.push("<br />");
+    projects.push(
+      "Most of the projects I have developed are for clients which are confidential. But still these are the one's which are available as open source repos in Github"
+    );
+
+    return projects;
+  } catch (error) {
+    console.log(error);
+    return projectsError;
+  }
+}
+
+async function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "help":
+      loopLines(help, "color2 margin", 80);
+      break;
+    case "about":
+      loopLines(about, "color2 margin", 80);
+      break;
+    case "video":
+      addLine("Opening YouTube...", "color2", 80);
+      newTab(youtube);
+      break;
+    case "projects":
+      loopLines(projectsLoading, "color2 margin", 80);
+
+      setTimeout(async () => {
+        const data = await getGithubRepos();
+        loopLines(data, "color2 margin", 80);
+      }, 200);
+
+      break;
+    case "history":
+      addLine("<br>", "", 0);
+      loopLines(commands, "color2", 80);
+      addLine("<br>", "command", 80 * commands.length + 50);
+      break;
+    case "email":
+      addLine(
+        'Opening mailto:<a href="mailto:satheshrgs@gmail.com">satheshrgs@gmail.com</a>...',
+        "color2",
+        80
+      );
+      newTab(email);
+      break;
+    case "clear":
+      setTimeout(function () {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "banner":
+      loopLines(banner, "color2", 80);
+      break;
+    // socials
+    case "twitter":
+      addLine("Opening Twitter...", "color2", 0);
+      newTab(twitter);
+      break;
+    case "social":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "linkedin":
+      addLine("Opening LinkedIn...", "color2", 0);
+      newTab(linkedin);
+      break;
+    case "instagram":
+      addLine("Opening Instagram...", "color2", 0);
+      newTab(instagram);
+      break;
+    case "github":
+      addLine("Opening GitHub...", "color2", 0);
+      newTab(github);
+      break;
+    default:
+      addLine(
+        '<span class="inherit">Command not found. For a list of commands, type <span class="command">\'help\'</span>.</span>',
+        "error",
+        100
+      );
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function () {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function () {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function (item, index) {
+    addLine(item, style, index * time);
+  });
+}
